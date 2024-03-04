1d250163c109b6b1ad71ec0dfa7b1a4238c588c4
GurungRegina <gina524286@gmail.com>
2024-03-04 19:56:26 +0000

    Update test_Petitions.py
    fix typos. remove unittest reference

--- unit_tests/tests/test_Petitions.py
+++ unit_tests/tests/test_Petitions.py
@@ -4,14 +4,12 @@ import os
 sys.path.append(f"{os.getcwd()}/src")
 
 import pytest
-import unittest
 
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 
-from framework_functions import read_in_json, get_top_20_columns
-import framework_functions
+from petitions_main import read_in_json, get_top_20_columns
 
 @pytest.fixture(autouse=True)
 def spark():
@@ -27,22 +25,18 @@ def spark():
 
 @pytest.mark.usefixtures("spark")
 class TestPetitions:
-    INPUT_PATH = f"{os.getcwd()}/data/input_data_test.json"
+      INPUT_PATH = f"{os.getcwd()}/data/input_data_test.json"
 
-    def test_read_in_json(self, spark):        
-        df = read_in_json(self.INPUT_PATH, spark)
+      def test_read_in_json(self, spark):
+          df = read_in_json(self.INPUT_PATH, spark)
 
-        assert len(df.columns) > 0
-        assert len(df.columns) == 2
+          assert not df.isEmpty()
+          assert len(df.columns) == 2
 
-    def test_get_top_20_columns(self, spark):
-        df = read_in_json(self.INPUT_PATH, spark)
+      def test_get_top_20_columns(self, spark):
+          df = read_in_json(self.INPUT_PATH, spark)
+          column_list = get_top_20_columns(df)
 
-        column_list = get_top_20_columns(df)
+          assert isinstance(column_list, list)
+          assert len(column_list) == 20
 
-        assert isinstance(column_list, list)
-        assert len(column_list) == 20
-
-    
-if __name__ == '__main__':
-    unittest.main()
